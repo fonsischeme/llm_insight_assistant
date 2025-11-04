ec0d46b209facbcedcf49bfec236018742e6f406
FB <alfonso.e.bonilla@gmail.com>
2025-11-04 12:39:13 -0800

    Update: Bugs

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -9,9 +9,14 @@ cfg = safe_load(open(CFG_PATH))
 
 def get_chroma_client(persist_directory=None):
     persist_directory = persist_directory or cfg["vector_store"]["persist_directory"]
+
+    if persist_directory and not os.path.exists(persist_directory):
+        os.makedirs(persist_directory)
+
     settings = Settings(
-        chroma_db_impl="duckdb+parquet",
-        persist_directory=persist_directory
+        #chroma_db_impl="duckdb+parquet",  # local storage
+        persist_directory=persist_directory,
+        #anonymized_telemetry=False
     )
     client = chromadb.Client(settings=settings)
     return client
